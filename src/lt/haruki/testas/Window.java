0bb8c40ad3fc43acecd6e9f41997b86e71a0c803
Haruki1 <ltgameplaytv@gmail.com>
2017-10-31 14:38:48 +0200

    Started working on menu bar

--- src/lt/haruki/testas/Window.java
+++ src/lt/haruki/testas/Window.java
@@ -2,14 +2,21 @@ package lt.haruki.testas;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.SwingConstants;
 
 public class Window {
 	private JFrame frame;
+	private JMenuBar menuBar;
 	private JLabel label;
 	private JTable table;
 	private int width;
@@ -26,6 +33,7 @@ public class Window {
 
 	private void InitializeWindow() {
 		frame = new JFrame();
+		menuBar = new JMenuBar();
 		frame.setLayout(new FlowLayout());
 		label = new JLabel("", SwingConstants.CENTER);
 		table = new JTable();
@@ -35,9 +43,50 @@ public class Window {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLocationRelativeTo(null);
 		
+		JMenu file = new JMenu("File");
+		file.setMnemonic(KeyEvent.VK_F);
+		menuBar.add(file);
+		JMenu edit = new JMenu("Edit");
+		file.setMnemonic(KeyEvent.VK_E);
+		menuBar.add(edit);
+		JMenu view = new JMenu("View");
+		file.setMnemonic(KeyEvent.VK_V);
+		menuBar.add(view);
+		JMenu help = new JMenu("Help");
+		file.setMnemonic(KeyEvent.VK_H);
+		menuBar.add(help);
+		
+		JMenuItem item1 = new JMenuItem("New");
+		JMenuItem item2 = new JMenuItem("Open");
+		JMenuItem item3 = new JMenuItem("Save");
+		JMenuItem item4 = new JMenuItem("Save As...");
+		JMenuItem item5 = new JMenuItem("Exit");
+		item1.setMnemonic(KeyEvent.VK_N);
+		item2.setMnemonic(KeyEvent.VK_O);
+		item3.setMnemonic(KeyEvent.VK_S);
+		item4.setMnemonic(KeyEvent.VK_A);
+		item5.setMnemonic(KeyEvent.VK_E);
+		item1.setToolTipText("New Table");
+		item2.setToolTipText("Open Table");
+		item3.setToolTipText("Save Table");
+		item4.setToolTipText("Save Table as SQL file");
+		item5.setToolTipText("Exit application");
+		item5.addActionListener((ActionEvent event) -> {
+			System.exit(0);
+		});
+		
+	    file.add(item1);
+	    file.add(item2);
+	    file.add(item3);
+	    file.add(item4);
+	    file.add(item5);
+	        
 		frame.add(label);
+		frame.setJMenuBar(menuBar);
 		
 		frame.setVisible(true);
+		
+		
 		
 	}
 	
